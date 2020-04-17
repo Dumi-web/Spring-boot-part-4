668a7effa892a056f5b5ab696d43f37cb6b5be90
Dumi-web <57130669+Dumi-web@users.noreply.github.com>
2020-04-17 14:30:40 +0200

    Update UserServicesImpl.java

--- src/main/java/com/dumisani/spring/part1/UserServicesImpl.java
+++ src/main/java/com/dumisani/spring/part1/UserServicesImpl.java
@@ -4,19 +4,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServicesImpl implements UserServices {
-    static FakeRepo repo = new FakeRepo();
+    @Autowired
+    FakeRepo fakeRepo;
     @Override
     public String addUser(long Id , String name, String surname) {
-        return repo.insertUser(1,name,surname);
+        fakeRepo = new FakeRepo();
+        return fakeRepo.insertUser(1,name,surname);
     }
     @Override
     public String getUser(long Id) {
-        return repo.findUserById(1);
+        fakeRepo = new FakeRepo();
+        return fakeRepo.findUserById(1);
     }
 
     @Override
     public String removeUser(long Id) {
-        return repo.deleteUser(1);
+        fakeRepo = new FakeRepo();
+        return fakeRepo.deleteUser(1);
     }
 
 
