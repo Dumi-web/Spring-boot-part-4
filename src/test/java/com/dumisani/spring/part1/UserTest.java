5e9e10ba3f73e010d3e2a510105eaf434dcccd28
Dumi-web <57130669+Dumi-web@users.noreply.github.com>
2020-05-24 18:49:09 +0200

    Update UserTest.java

--- src/test/java/com/dumisani/spring/part1/UserTest.java
+++ src/test/java/com/dumisani/spring/part1/UserTest.java
@@ -1,28 +1,42 @@
 package com.dumisani.spring.part1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URL;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-public class UserTest {
-    @Autowired
-    public UserServicesImpl user;
-    @Test
-    public void addUser(){
-        String newName = user.addUser(1,"Dumisani","Ngobeni");
-        assertThat(newName).isEqualTo("Dumisani Entered");
-    }
-    @Test
-    public void getUser(){
-        String name = user.getUser(1);
-        assertThat(name).isEqualTo("Hello Dumisani Ngobeni");
-    }
-    @Test
-    public void removeUser(){
-        String Nam = user.removeUser(1);
-        assertThat(Nam).isEqualTo("Dumisani Removed");
-    }
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    public class UserTest {
+        @LocalServerPort
+        private int port;
+        @Autowired
+        public TestRestTemplate template;
+        @Autowired
+        public UserServices userServices;
+
+        private URL base;
+
+        @BeforeEach
+        public void urlLink()throws Exception{
+            System.out.println(port);
+            this.base =  new URL("http://localhost:" + port);
+        }
+        @Test
+    public void getGit(){
+            ResponseEntity<String> test = userServices.getGit();
+            System.out.println(test);
+        }
+        @Test
+    public void getCommits(){
+            ResponseEntity<String> name1 = userServices.getCommits();
+            System.out.println(name1);
+        }
 
 }
